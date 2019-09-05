e6180fc78d6103664e90b04497dc1bdac9e8d444
ameng <alexbmeng@gmail.com>
2019-09-04 21:46:41 -0600

    Remove comment

--- terraform/service_lb.sh
+++ terraform/service_lb.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# Get a list of GitHub webhook IPs and return the result as JSON
-# This is used to whitelist GitHub webhooks in security groups.
-#
-# Terraform only allows string key/value pairs in the JSON response,
-# so we return the IPs as a comma-delimited string.
-#
-# Example:
-# {"ips": "1.2.3.4/22,5.6.7.8/22"}
-
 set -Eeo pipefail
 
 LB_NAME=$(echo $X | cut -d "-" -f 1)
