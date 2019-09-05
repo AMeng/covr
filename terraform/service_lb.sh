4b9cb91e48e795a878ec908636696c1a4aea93c5
ameng <alexbmeng@gmail.com>
2019-09-04 21:51:35 -0600

    Fix variable name

--- terraform/service_lb.sh
+++ terraform/service_lb.sh
@@ -2,7 +2,7 @@
 
 set -Eeo pipefail
 
-LB_NAME=$(echo $X | cut -d "-" -f 1)
+LB_NAME=$(echo $1 | cut -d "-" -f 1)
 LB_ARN=$(aws elbv2 describe-load-balancers --names $LB_NAME | jq -r '.LoadBalancers[].LoadBalancerArn')
 TG_ARN=$(aws elbv2 describe-target-groups --load-balancer-arn $LB_ARN | jq -r '.TargetGroups[].TargetGroupArn')
 
